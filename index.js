6b74680e7f621bc143a2ac5fe899e1481a3d4721
Abdulbaloch78600 <bk0109685@gmail.com>
2025-10-29 21:11:15 +0000

    Update latest

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ var http = require("http");
 const port = 8000; 
 
 http.createServer(function(req, res) { 
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("<!doctype html><html1><head><title>MYPage</title>")
-    }).listen(port, function() { 
-        console.log(`Node server is running on port ${port}...`); 
-}); 
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(`<!doctype html>
+<html>
+<head>
+    <title>MYPage</title>
+</head>
+<body>
+    <h1>Welcome to MYPage!</h1>
+</body>
+</html>`);
+}).listen(port, function() { 
+    console.log(`Node server is running on port ${port}...`); 
+});
